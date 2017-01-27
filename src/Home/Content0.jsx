7c21d24ef29d96657478462bca9114b7c6939e1d
yufujia <fujiazhiyu@sina.com>
2017-01-27 14:06:06 +0800

    caozhehui's website of works v1.0 mainpage finished

---
+++ src/Home/Content0.jsx
@@ -0,0 +1,59 @@
+import React, { PropTypes } from 'react';
+import { Button, Icon } from 'antd';
+import QueueAnim from 'rc-queue-anim';
+import TweenOne from 'rc-tween-one';
+import OverPack from 'rc-scroll-anim/lib/ScrollOverPack';
+
+var firstURL = 'http://media.caozhehui.cn/caozhehui_works/assets/circle.png';
+
+class Content extends React.Component {
+  render() {
+    return (
+      <OverPack
+        replay
+        playScale={[0.3, 0.1]}
+        {...this.props}
+        style={{ backgroundImage: '', backgroundColor: '#ea0909', borderStyle: 'none', borderColor: '#dc1515', borderWidth: '0px 0px 0px 2px', backgroundSize: 'cover', backgroundBlendMode: 'color-dodge' }}
+        hideProps={{ icon: { reverse: true } }}
+      >
+        <QueueAnim
+          type={['bottom', 'top']}
+          delay={200}
+          className={`${this.props.className}-wrapper`}
+          key="text"
+        >
+          <span
+            className="title"
+            key="title"
+            style={{ color: '#f3f2f2' }}
+          >
+            <img width="200px" height="200px" src={firstURL} alt='firstURL'/>
+          </span>
+          <p
+            key="content"
+            style={{ color: '#fefefe' }}
+          >
+            ZHEHUI CAO
+          </p>
+        </QueueAnim>
+        <TweenOne
+          animation={{ y: '-=20', yoyo: true, repeat: -1, duration: 1000 }}
+          className={`${this.props.className}-icon`}
+          key="icon"
+        >
+          <Icon type="down" />
+        </TweenOne>
+      </OverPack>
+    );
+  }
+}
+
+Content.propTypes = {
+  className: PropTypes.string,
+};
+
+Content.defaultProps = {
+  className: 'banner0',
+};
+
+export default Content;
